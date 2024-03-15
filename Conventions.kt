ef34649671b922222475b05a01941fad2e42acc3
vvnuts <142222258+vvnuts@users.noreply.github.com>
2024-03-15 13:20:28 +0300

    Add files via upload

---
+++ Conventions.kt
@@ -0,0 +1,77 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    // this code should compile:
+    println(date1 < date2)
+}
+
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    return date in first..last
+}
+
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current: MyDate = start
+
+            override fun next(): MyDate {
+                if (!hasNext()) throw NoSuchElementException()
+                val result = current
+                current = current.followingDate()
+                return result
+            }
+
+            override fun hasNext(): Boolean = current <= end
+        }
+    }
+}
+
+fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
+    for (date in firstDate..secondDate) {
+        handler(date)
+    }
+}
+
+import TimeInterval.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+// Supported intervals that might be added to dates:
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+operator fun MyDate.plus(timeInterval: TimeInterval) =
+    addTimeIntervals(timeInterval, 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) =
+    RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) =
+    addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
+
+fun task1(today: MyDate): MyDate {
+    return today + YEAR + WEEK
+}
+
+fun task2(today: MyDate): MyDate {
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
+}
+
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
